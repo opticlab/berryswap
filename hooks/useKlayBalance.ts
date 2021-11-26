b2a8075c1f71112fe612452a9c398752a31bcaf6
Kay <chief@opticlab.org>
2021-11-26 11:52:04 +0900

    refactor: minor code change

--- hooks/useKlayBalance.ts
+++ hooks/useKlayBalance.ts
@@ -1,19 +1,20 @@
 import { BigNumber } from "ethers";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { getKlayBalance } from "../utils/caver";
 import { useActiveCaverReact } from "./useActiveCaverJsReact";
 
 export function useKlayBalance() {
   const { account } = useActiveCaverReact();
   const [balance, setBalance] = useState(BigNumber.from(0));
+
   useEffect(() => {
-    if (account) {
-      const fetchBalance = async () => {
+    const fetchBalance = async () => {
+      if (account) {
         setBalance(await getKlayBalance(account));
-      };
+      }
 
       fetchBalance();
-    }
+    };
   }, [account]);
 
   return balance;
